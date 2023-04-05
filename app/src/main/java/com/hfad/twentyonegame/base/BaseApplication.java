0f1bfbf4bdd18dd99590d1a8a3606f1011b77b90
Najimkhon <qwerty1995.290@gmail.com>
2023-04-05 10:35:39 +0500

    Dependency injection is set up

---
+++ app/src/main/java/com/hfad/twentyonegame/base/BaseApplication.java
@@ -0,0 +1,9 @@
+package com.hfad.twentyonegame.base;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class BaseApplication extends Application {
+}
--- app/src/main/java/com/hfad/twentyonegame/di/AppModule.java
+++ app/src/main/java/com/hfad/twentyonegame/di/AppModule.java
@@ -1,4 +1,10 @@
 package com.hfad.twentyonegame.di;
 
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
 public class AppModule {
 }
