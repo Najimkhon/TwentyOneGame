1894e989e727db6b14b3f00456dcd2f4ff3d6420
Najimkhon <qwerty1995.290@gmail.com>
2023-04-05 21:05:54 +0500

    Create GameViewModel.java

---
+++ app/src/main/java/com/hfad/twentyonegame/ui/viewmodels/GameViewModel.java
@@ -0,0 +1,10 @@
+package com.hfad.twentyonegame.ui.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
+public class GameViewModel extends ViewModel {
+
+}
