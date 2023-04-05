3d9793ee831252876de87fdc212e9b72899cb609
Najimkhon <qwerty1995.290@gmail.com>
2023-04-05 21:49:17 +0500

    Player model is added

---
+++ app/src/main/java/com/hfad/twentyonegame/models/Player.java
@@ -0,0 +1,10 @@
+package com.hfad.twentyonegame.models;
+
+import androidx.annotation.DrawableRes;
+
+public class Player {
+    private String name;
+    private int score;
+    @DrawableRes
+    private int avatarRes;
+}
